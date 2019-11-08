31c5d70bdcff9e22e05c27793cdbef4fd67f679a
Lilouschka <l.garten@t-online.de>
2019-11-08 11:34:28 +0100

    Revert "Binding Data Trial"
    This reverts commit 2a8fe59efaf59808049913d3457620b5fb88f1e7.

--- src/components/IndexCard/IndexCardHandler.vue
+++ src/components/IndexCard/IndexCardHandler.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="indexCardHandler">
-    <IndexCard v-for="n in 10" :key="n"></IndexCard>
+    <IndexCard v-for="n in 10" v-bind:key="n"></IndexCard>
   </div>
 </template>
 
@@ -12,16 +12,7 @@ export default {
   components: {
     IndexCard
   },
-  props: {
-    incomingData: {
-      type: Object,
-      required: true
-    }
-  },
-  created: function() {
-    console.log(this.incomingData);
-    //console.log(this.incomingData.status[0].entries);
-  }
+  props: {}
 };
 </script>
 
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="overview">
     <ToolBar />
-    <IndexCardHandler :incoming-data="importedData" />
+    <IndexCardHandler />
   </div>
 </template>
 
@@ -9,19 +9,12 @@
 //@ is an alias to /src
 import ToolBar from "@/components/ToolBar/ToolBar.vue";
 import IndexCardHandler from "@/components/IndexCard/IndexCardHandler.vue";
-import originData from "@/sampleData.json";
 
 export default {
   name: "Overview",
   components: {
     ToolBar,
     IndexCardHandler
-  },
-
-  data() {
-    return {
-      importedData: originData
-    };
   }
 };
 </script>
