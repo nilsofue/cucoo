08a615ba5fc78afd96d5d423b50575ab19499e1e
Nils <nilsofue@googlemail.com>
2019-11-09 14:25:44 +0100

    search implementation for city and street

--- src/components/ToolBar/ToolBar.vue
+++ src/components/ToolBar/ToolBar.vue
@@ -2,7 +2,7 @@
   <div class="toolBar">
     <b-navbar toggleable="lg" type="primary" variant="light">
       <b-navbar-nav>
-        <b-nav-item-dropdown :text="selectedStatus">
+        <b-nav-item-dropdown :text="selectedStatusName">
           <b-dropdown-item
             v-for="(status, i) in statusData"
             :key="i"
@@ -14,7 +14,29 @@
 
         <b-nav-form>
           <b-nav-text>Straße:</b-nav-text>
-          <b-form-input size="sm" class="mr-sm-2"></b-form-input>
+          <b-form-input
+            v-model="streetSearchValue"
+            size="sm"
+            class="mr-sm-2"
+            @keyup="handleSearch()"
+          ></b-form-input>
+        </b-nav-form>
+        <b-nav-form>
+          <b-nav-text>Ort:</b-nav-text>
+          <b-form-input v-model="citySearchValue" size="sm" class="mr-sm-2" @keyup="handleSearch()"></b-form-input>
+        </b-nav-form>
+        <b-nav-form>
+          <b-nav-text>Termin:</b-nav-text>
+          <b-form-input v-model="dateSearchValue" size="sm" class="mr-sm-2" @keyup="handleSearch()"></b-form-input>
+        </b-nav-form>
+        <b-nav-form>
+          <b-nav-text>Erster Besuch:</b-nav-text>
+          <b-form-input
+            v-model="firstVisitSearchValue"
+            size="sm"
+            class="mr-sm-2"
+            @keyup="handleSearch()"
+          ></b-form-input>
         </b-nav-form>
       </b-navbar-nav>
     </b-navbar>
@@ -34,7 +56,12 @@ export default {
   },
   data() {
     return {
-      selectedStatus: "Status"
+      selectedStatusName: "Status",
+      selectedStatusData: null,
+      streetSearchValue: "",
+      citySearchValue: "",
+      dateSearchValue: "",
+      firstVisitSearchValue: ""
     };
   },
   mounted: function() {
@@ -45,8 +72,38 @@ export default {
   },
   methods: {
     handleSelection(selectedData) {
-      this.selectedStatus = selectedData.name;
+      console.log("clear");
+      //clear search inputs
+      this.streetSearchValue = "";
+      this.citySearchValue = "";
+      this.dateSearchValue = "";
+      this.firstVisitSearchValue = "";
+
+      this.selectedStatusData = selectedData;
+      this.selectedStatusName = selectedData.name;
       this.$emit("update-Index-Cards", selectedData.entries);
+    },
+    handleSearch() {
+      this.$emit(
+        "update-Index-Cards",
+        this.selectedStatusData.entries.filter(this.filterElements)
+      );
+    },
+    filterElements(indexCardData) {
+      console.log(indexCardData);
+      if (
+        indexCardData.adress.city
+          .toLowerCase()
+          .indexOf(this.citySearchValue.toLowerCase()) === -1
+      )
+        return false;
+      if (
+        indexCardData.adress.street
+          .toLowerCase()
+          .indexOf(this.streetSearchValue.toLowerCase()) === -1
+      )
+        return false;
+      return true;
     }
   }
 };
