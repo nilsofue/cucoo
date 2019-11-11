823a7942da87411e3cebd9e862435d92151e9abe
Nils <nilsofue@googlemail.com>
2019-11-11 14:02:49 +0100

    add basic functions to data handler

--- src/DataHandler.js
+++ src/DataHandler.js
@@ -1,9 +1,21 @@
-import inputData from "./assets/sampleData.json";
+import inputData from "./assets/sampleData.json"; //only for testing
 
 export default new (class {
   constructor() {}
 
   getData() {
-    return inputData;
+    return inputData; //only for testing
+  }
+
+  changeData(dataSetId, statusId, indexCardId) {
+    console.log("Change data" + dataSetId, statusId, indexCardId);
+  }
+
+  addData(dataSetId, statusId, indexCardId) {
+    console.log("Add data" + dataSetId, statusId, indexCardId);
+  }
+
+  deleteData(dataSetId, statusId, indexCardId) {
+    console.log("Delete data" + dataSetId, statusId, indexCardId);
   }
 })();
