b3dca6df994bd50c7d4ef34795faeed4b287dd2a
Nils <nilsofue@googlemail.com>
2019-11-18 14:38:05 +0100

    working on file system

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,26 @@
 var express = require("express");
 var router = express.Router();
+const fs = require("fs");
+const dataSetPath = "/server_data/dataset.json";
 
 let inputData = require("../src/assets/sampleData.json"); //only for testing
 
+function fileExists() {
+  if (fs.existsSync(dataSetPath)) {
+    return true;
+  }
+  return false;
+}
+
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/data", function(req, res, next) {
+  console.log(fileExists());
   res.json(inputData);
+});
+
+router.post("/dataPost", function(req, res) {
+  console.log(req.body);
+  res.send("Alles super!");
 });
 
 module.exports = router;
