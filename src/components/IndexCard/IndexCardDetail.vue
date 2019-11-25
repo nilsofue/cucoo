3a84366c4384f1478bed3be39c5f5afff04bf71b
Lilouschka <l.garten@t-online.de>
2019-11-25 11:51:56 +0100

    modal created to show content new component IndexCardDetail

---
+++ src/components/IndexCard/IndexCardDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="indexCardDetail">
+    <b-modal :id="indexCardData.id" :title="indexCardData.company">
+      <p>
+        <br />
+        {{ indexCardData.adress.street }} {{ indexCardData.adress.houseNumber }}
+        <br />
+        {{ indexCardData.adress.postCode }} {{ indexCardData.adress.city }}
+      </p>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexCardDetail",
+  props: {
+    indexCardData: Object
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
