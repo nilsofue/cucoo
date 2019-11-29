e12edffdf952342a04e2a5702461775ba4452ae5
Nils <nilsofue@googlemail.com>
2019-11-29 17:20:11 +0100

    backend read from disk

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
 var express = require("express");
 var router = express.Router();
-const fs = require("fs");
-const dataSetPath = "/server_data/dataset.json";
+const fs = require("fs").promises;
+const dataSetPath = "./routes/server_data/dataset.json";
 
-let inputData = require("../src/assets/sampleData.json"); //only for testing
-
-function fileExists() {
-  if (fs.existsSync(dataSetPath)) {
-    return true;
-  }
-  return false;
-}
-
-/* GET home page. */
 router.get("/data", function(req, res, next) {
-  console.log(fileExists());
-  res.json(inputData);
+  loadUserData()
+    .then(inputData => {
+      res.json(JSON.parse(inputData));
+    })
+    .catch(err => {
+      console.log("Error reading file from disk:", err);
+      if (err.errno == -2) {
+        createNewUserData();
+        loadUserData().then(inputData => {
+          res.json(JSON.parse(inputData));
+        });
+      }
+    });
 });
 
 router.post("/dataPost", function(req, res) {
@@ -24,3 +25,44 @@ router.post("/dataPost", function(req, res) {
 });
 
 module.exports = router;
+
+const loadUserData = async () => {
+  const content = await fs.readFile(dataSetPath, "utf8");
+  return content;
+};
+
+function createNewUserData() {
+  const userData = {
+    timestamp: new Date().getTime(),
+    id: uuidv4(),
+    name: "Max Mustermanns Kunden-Dashboard",
+    status: []
+  };
+  const jsonString = JSON.stringify(userData);
+  fs.writeFile(dataSetPath, jsonString, err => {
+    if (err) {
+      console.log("Error writing file", err);
+    } else {
+      console.log("Successfully wrote file");
+    }
+  });
+}
+
+function updateUserData(userData) {
+  const jsonString = JSON.stringify(userData);
+  fs.writeFile(dataSetPath, jsonString, err => {
+    if (err) {
+      console.log("Error writing file", err);
+    } else {
+      console.log("Successfully wrote file");
+    }
+  });
+}
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
