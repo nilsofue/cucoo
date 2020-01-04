57d32e1a736c86596c5c3d9362f1f5097cf8552e
Nils <df>
2020-01-04 18:36:32 +0100

    add colored status chips to index cards

--- src/components/IndexCard/IndexCard.vue
+++ src/components/IndexCard/IndexCard.vue
@@ -33,14 +33,24 @@
         <div class="dateTable">
           <table>
             <tr>
-              <td class="iconColumn">
+              <td class="iconColumnBottom">
                 <font-awesome-icon icon="clock" />
               </td>
-              <td class="dataColumn">
+              <td class="dataColumnBottom">
                 {{ getDateString(indexCardData.date) }}
               </td>
             </tr>
           </table>
+        </div>
+        <div
+          class="statusTag"
+          :style="{
+            backgroundColor:
+              getStatusDataByIndexCardId(indexCardData.id).color + '30',
+            color: getStatusDataByIndexCardId(indexCardData.id).color
+          }"
+        >
+          {{ getStatusDataByIndexCardId(indexCardData.id).name }}
         </div>
       </b-card-text>
     </b-card>
@@ -50,6 +60,7 @@
 
 <script>
 import IndexCardDetail from "@/components/IndexCard/IndexCardDetail.vue";
+import { mapGetters } from "vuex";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPhone, faMapPin, faClock } from "@fortawesome/free-solid-svg-icons";
 
@@ -61,9 +72,15 @@ export default {
     IndexCardDetail
   },
   props: {
-    indexCardData: Object
+    indexCardData: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
   },
-  computed: {},
+  computed: {
+    ...mapGetters(["getStatusDataByIndexCardId"])
+  },
   methods: {
     getDateString: function(date) {
       date = new Date(date);
@@ -149,16 +166,42 @@ export default {
 
 .iconColumn {
   width: 26px;
-  color: grey;
+  color: rgb(161, 161, 161);
   font-size: 17px;
 }
 
 .dataColumn {
   font-size: 15px;
+  color: rgb(82, 82, 82);
+  letter-spacing: 0.5px;
 }
 
 .dateTable {
   position: absolute;
   bottom: 15px;
+}
+
+.dataColumnBottom {
+  font-size: 15px;
+}
+
+.iconColumnBottom {
+  width: 26px;
+  color: grey;
+  font-size: 17px;
+}
+
+.dateTable table {
+  width: 100%;
+}
+
+.statusTag {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  padding: 4px;
+  font-size: 0.7em;
+  border-radius: 4px;
+  font-weight: 600;
 }
 </style>
