aae6f4a3e1cea68fbce554dc90ee7804fbbcb1bc
Nils <df>
2020-01-15 17:27:59 +0100

    add color picker

--- src/components/NewStatus/NewStatus.vue
+++ src/components/NewStatus/NewStatus.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="newStatus">
-    <b-modal
-      id="createNewStatus"
-      title="Neuen Status anlegen"
-      hide-footer
-    ></b-modal>
+    <b-modal id="createNewStatus" title="Neuen Status anlegen" hide-footer>
+      <div>
+        <table>
+          <tr>
+            <td>Bezeichnung:</td>
+            <td>
+              <b-form-input
+                v-model="statusName"
+                placeholder="Bezeichnung eingeben"
+              ></b-form-input>
+            </td>
+          </tr>
+          <tr>
+            <td>Farbe des Status:</td>
+            <td>
+              <verte picker="wheel" value="#f0f"></verte>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </b-modal>
   </div>
 </template>
 
 <script>
+import Verte from "verte";
+import "verte/dist/verte.css";
+
 export default {
   name: "NewStatus",
-  components: {}
+  components: { Verte },
+  data() {
+    return {
+      statusName: ""
+    };
+  }
 };
 </script>
 
