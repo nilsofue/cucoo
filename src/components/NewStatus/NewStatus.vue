50d05fe720c5c1cb801da10bff836df85259e1a3
Nils <df>
2020-01-15 14:22:13 +0100

    implement button for creation and add component

---
+++ src/components/NewStatus/NewStatus.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="newStatus">
+    <b-modal
+      id="createNewStatus"
+      title="Neuen Status anlegen"
+      hide-footer
+    ></b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewStatus",
+  components: {}
+};
+</script>
+
+<style></style>
